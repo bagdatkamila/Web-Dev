7e8625cbabd57d05ce543274e57ab8da3fefbf6e
bagdatkamila <kamillabagdat@gmail.com>
2024-02-17 13:18:58 +0600

    lab4

---
+++ lab4/src/app/products.ts
@@ -0,0 +1,97 @@
+export interface Product {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+  }
+  
+  export const products = [
+    {
+      id: 1,
+      name: 'Anua тонер Heartleaf 77%',
+      price: 7158,
+      description: 'Anua тонер Heartleaf 77% Soothing Toner 250 мл',
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/h00/h34/84993668907038.jpg?format=gallery-medium',
+      link: 'https://kaspi.kz/shop/p/anua-toner-heartleaf-77-soothing-toner-250-ml-113961265/?c=750000000'
+
+    },
+    {
+      id: 2,
+      name: "Guerlain L'Instant de",
+      price: 500000,
+      description: "Guerlain L'Instant de Guerlain парфюмерная вода EDP 100 мл, для женщин",
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/h53/h68/64004185456670.jpg?format=gallery-medium',
+      link: 'https://kaspi.kz/shop/p/guerlain-l-instant-de-guerlain-parfjumernaja-voda-edp-100-ml-dlja-zhenschin-100472816/?c=750000000'
+    },
+    {
+      id: 3,
+      name: 'Печенье Oreo',
+      price: 599,
+      description: 'Печенье Oreo Original с какао и начинкой с ванильным вкусом 228 г',
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/h26/h16/84983017209886.jpg?format=gallery-medium',
+      link: 'https://kaspi.kz/shop/p/pechen-e-oreo-original-s-kakao-i-nachinkoi-s-vanil-nym-vkusom-228-g-100971349/?c=750000000'
+    },
+    {
+      id: 4,
+      name: 'LEGO flowers',
+      price: 40000,
+      description: '',
+      img: '',
+      link: 'https://kaspi.kz/shop/p/lego-buket-tsvetov-10280-detalei-756-sht-101204989/?c=750000000'
+    },
+    {
+      id: 5,
+      name: 'Nike purple',
+      price: 39920,
+      description: '',
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/h60/ha1/84634294517790.jpg?format=gallery-large',
+      link: 'https://kaspi.kz/shop/p/nike-dv5477-500-fioletovyi-39-115052784/?c=750000000'
+    },
+    {
+      id: 6,
+      name: 'Sony PlayStation 5',
+      price: 277051,
+      desciption: '',
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/h02/h59/63948652249118.jpg?format=gallery-medium',
+      link: 'https://kaspi.kz/shop/p/sony-playstation-5-belyi-100746577/?c=750000000'
+    },
+    {
+      id: 7,
+      name: 'Агуша',
+      price: 299,
+      description: 'Детское пюре Агуша Яблоко-клубника-малина 90 г',
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/h53/hc1/63957671575582.jpg?format=gallery-large',
+      link: 'https://kaspi.kz/shop/p/agusha-jabloko-klubnika-malina-90-g-100623509/?c=750000000'
+    },
+    {
+      id: 8,
+      name: 'Dyson V8 Absolute',
+      price: 229910,
+      description: 'Пылесос Dyson V8 Absolute серый',
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/ha9/h96/67358382293022.jpg?format=gallery-medium',
+      link: 'https://kaspi.kz/shop/p/dyson-v8-absolute-seryi-108244947/?c=750000000'
+    },
+    {
+      id: 9,
+      name: 'Наушники Marshall',
+      price: 33581,
+      description: 'Наушники Marshall Major IV черный',
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/h20/h21/64344048762910.jpg?format=gallery-medium',
+      link: 'https://kaspi.kz/shop/p/marshall-major-iv-chernyi-102138144/?c=750000000'
+    },
+    {
+      id: 10,
+      name: 'Milka ореховая паста',
+      price: 2750,
+      description: 'Milka Ореховая с добавлением какао 350 г',
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/ha3/h70/68919316512798.jpg?format=gallery-medium',
+      link: 'https://kaspi.kz/shop/p/milka-orehovaja-s-dobavleniem-kakao-350-g-102295465/?c=750000000'
+    },
+  ];
+  
+  
+  /*
+  Copyright Google LLC. All Rights Reserved.
+  Use of this source code is governed by an MIT-style license that
+  can be found in the LICENSE file at https://angular.io/license
+  */
